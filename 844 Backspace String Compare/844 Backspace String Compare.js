f95d0a162cb11fd5451a0703e53831b0bea5bb99
YenLin01 <134580412+YenLin01@users.noreply.github.com>
2023-10-19 16:03:05 +0800

    new solve

--- 844 Backspace String Compare/844 Backspace String Compare.js
+++ 844 Backspace String Compare/844 Backspace String Compare.js
@@ -19,4 +19,17 @@ function check(str) {
 var backspaceCompare = function (s, t) {
   return check(s) == check(t);
 };
-console.log(1213);
+
+function check1(str) {
+  for (i = 0; i < str.length; i++) {
+    if (str[i] == "#") {
+      console.log(i - 1);
+      return check(str.slice(0, i - 1) + str.slice(i + 1));
+    }
+  }
+  return str;
+}
+
+str = "#123";
+console.log(str.slice(0, -1));
+// console.log(check1(str));
